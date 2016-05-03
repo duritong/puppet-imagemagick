ba04c047534789384d3550f793d32f75cf88b141
o <o@immerda.ch>
2016-05-03 23:17:23 +0200

    mitigate imagemagick 0-day
    https://imagetragick.com/

--- manifests/base.pp
+++ manifests/base.pp
@@ -3,4 +3,18 @@ class imagemagick::base {
     package{'ImageMagick':
         ensure => present,
     }
+
+    file{
+      "/etc/ImageMagick":
+        content => '<policymap>
+            <policy domain="coder" rights="none" pattern="EPHEMERAL" />
+            <policy domain="coder" rights="none" pattern="URL" />
+            <policy domain="coder" rights="none" pattern="HTTPS" />
+            <policy domain="coder" rights="none" pattern="MVG" />
+            <policy domain="coder" rights="none" pattern="MSL" />
+          </policymap>',
+        owner   => root,
+        group   => root,
+        mode    => '0644';
+    }
 }
