655a016b46ec61dc20cb85476a6d9543a4c32780
mh <mh@immerda.ch>
2020-05-13 00:09:34 +0200

    not required anymore

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,12 +3,5 @@ class imagemagick {
   include avahi
   package{'ImageMagick':
       ensure => present,
-  } -> file{
-    '/etc/ImageMagick/policy.xml':
-      source => ['puppet:///modules/site_imagemagick/policy.xml',
-                 'puppet:///modules/imagemagick/policy.xml'],
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0444';
   }
 }
